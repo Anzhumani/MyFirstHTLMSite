53c4f6b84963fba2d26fa0b309fa57018e3003f2
psibia <psibia.p@gmail.com>
2024-11-10 15:53:45 +0700

    4324

--- resources/views/components/header3.blade.php
+++ resources/views/components/header3.blade.php
@@ -1,4 +1,4 @@
-<header class="rainbow-header header-default header-not-transparent header-sticky">
+<header class="rainbow-header header-default header-transparent header-sticky">
         <div class="container position-relative">
             <div class="row align-items-center row--0">
                 <div class="col-lg-3 col-md-6 col-6">
